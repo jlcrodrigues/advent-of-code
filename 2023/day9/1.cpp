4df7ec1b9bf798953af8fdac7a331716ab991c79
luis <joseluiscunharodrigues@gmail.com>
2023-12-09 10:37:09 +0000

    Day 9 2023

---
+++ 2023/day9/1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    long count = 0;
+    string s;
+    while (getline(cin, s))
+    {
+        int n;
+        vector<vector<int>> v;
+        v.push_back({});
+        stringstream ss(s);
+        while ((ss >> n)) v[0].push_back(n);
+
+        int isZero = false;
+        while (!isZero) {
+            isZero = true;
+            v.push_back({});
+            for (int i = 0; i < v[v.size() - 2].size() - 1; i++) {
+                int next = v[v.size() - 2][i + 1] - v[v.size() - 2][i];
+                if (next != 0) isZero = false;
+                v.back().push_back(next);
+            }
+        }
+        int cCount = 0;
+        for (int i = v.size() - 1; i >= 0; i--) {
+            cCount += v[i].back();
+        }
+        count += cCount;
+    }
+    cout << count << endl;
+    return 0;
+}
